afed55930a5c10c0f28a193ff35cadb9b38fba32
R.E. Moose <rachelemoose@gmail.com>
2019-03-23 12:53:13 -0400

    adding server

--- sms.js
+++ sms.js
@@ -3,21 +3,34 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
+const express = require('express');
+const app = express();
+var PORT = process.env.PORT || 3000;
 // this array needs to popoulate with hosts's contacts 
-const invitees = ['+15716436981', '+17187535298']
 
-let body = "Rachel Moose has sent you a PartyVite! Send 'Yes' if you can make it, 'No' if you can't attend, and 'Maybe', if you're not sure."
+app.post('/invite', (req, res) => {
+    const invitees = ['+15716436981', '+17187535298']
+    let body = "Rachel Moose has sent you a PartyVite! Send 'Yes' if you can make it, 'No' if you can't attend, and 'Maybe', if you're not sure."
+    Promise.all(
+    invitees.map(invitee => {
+        return client.messages.create({
+        to: invitee,
+        from: twilioNumber,
+        body: body
+        });
+    })
+    )
+    .then(messages => {
+        console.log('Messages sent!');
+    })
+    .catch(err => console.error(err));
+});
 
-Promise.all(
-  invitees.map(invitee => {
-    return client.messages.create({
-      to: invitee,
-      from: twilioNumber,
-      body: body
-    });
-  })
-)
-  .then(messages => {
-    console.log('Messages sent!');
-  })
-  .catch(err => console.error(err));
+
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  [WEB] Server listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
